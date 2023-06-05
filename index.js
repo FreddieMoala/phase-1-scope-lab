3e514638ebd40a2a7e59d10ad21cf7afcad90852
Freddie Moala <loto_toa@hotmail.com>
2023-06-05 20:02:09 +1000

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'Paul';
+}
+
+console.log(changeLeastFavoriteCustomer());
